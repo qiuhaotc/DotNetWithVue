a176cf994cfb996618f4161c9a3f50dcb857ddb0
qiuhaotc <qiu66hao@163.com>
2022-01-10 16:53:42 +0800

    done

---
+++ src/DotNetWithVue.Server/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWithVue.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HomeController : ControllerBase
+	{
+		readonly ILogger<HomeController> logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			this.logger = logger;
+		}
+
+		[HttpGet]
+		public DateTime Get()
+		{
+			return DateTime.Now;
+		}
+	}
+}
