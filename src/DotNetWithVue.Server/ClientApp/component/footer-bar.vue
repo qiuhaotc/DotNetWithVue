7a34f4e6f31678590e3054620a12e9db27d3bb97
qiuhaotc <qiu66hao@163.com>
2022-02-25 17:43:20 +0800

    tweaks

--- src/DotNetWithVue.Server/ClientApp/component/footer-bar.vue
+++ src/DotNetWithVue.Server/ClientApp/component/footer-bar.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="alignToCenter">
-		<el-link href="https://github.com/qiuhaotc/LearnVue" target="_blank"
+		<el-link
+			href="https://github.com/qiuhaotc/DotNetWithVue"
+			target="_blank"
 			>About this project</el-link
 		>
 	</div>
